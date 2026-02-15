5224c74d8c4c2a923d4d7d9fba7fdf555dc4ba63
agile-learning-institute <agile-learning-institute@users.noreply.github.com>
2026-02-15 01:48:11 +0000

    Template Merge Processing Complete

--- .stage0_template/test_expected/src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AutoSaveField } from './AutoSaveField.vue'
-export { default as AutoSaveSelect } from './AutoSaveSelect.vue'
-export { default as ListPageSearch } from './ListPageSearch.vue'
--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:{{ spa_utils.port }}',
+    baseUrl: 'http://localhost:8388',
     supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     video: false,
